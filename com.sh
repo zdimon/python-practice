f230b72face6dc9b2a14ce549ccee96425523c6d
zdimon <zdimon77@gmail.com>
2015-05-04 17:48:31 +0300

    commit at 04/05/2015_17:48:31

--- com.sh
+++ com.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
+comment=$1
 dt=`date '+%d/%m/%Y_%H:%M:%S'`
 git add --all
-git commit -m "commit at $dt"
-echo "commit at $dt"
+if [ -n "$comment" ]
+    then
+        git commit -m "commit at $dt"
+    else
+        git commit -m "$comment"
+fi
+git push  
